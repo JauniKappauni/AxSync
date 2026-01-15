2fa1c0a186ee47f622acd0c560d8274aeb78653d
JauniKappauni <social.jauni@gmail.com>
2026-01-15 17:33:14 +0100

    feat: add db connector and table initialization

--- src/main/java/de/jauni/axsync/AxSync.java
+++ src/main/java/de/jauni/axsync/AxSync.java
@@ -1,13 +1,27 @@
 package de.jauni.axsync;
 
+import de.jauni.axsync.manager.DatabaseManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxSync extends JavaPlugin {
+    DatabaseManager databaseManager;
+    public DatabaseManager getDatabaseManager() {
+        return databaseManager;
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        saveDefaultConfig();
+        try{
+            databaseManager = new DatabaseManager(this);
+            if(databaseManager.initDatabaseTables() == false){
+                getLogger().severe("Error creating playerData table!");
+                Bukkit.getServer().shutdown();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
---
+++ src/main/java/de/jauni/axsync/manager/DatabaseManager.java
@@ -0,0 +1,44 @@
+package de.jauni.axsync.manager;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+
+    HikariDataSource hikari;
+
+    public DatabaseManager(JavaPlugin plugin){
+        FileConfiguration pluginConfig = plugin.getConfig();
+
+        String url = pluginConfig.getString("database.url");
+        String username = pluginConfig.getString("database.username");
+        String password = pluginConfig.getString("database.password");
+
+        HikariConfig config = new HikariConfig();
+
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+
+        hikari = new HikariDataSource(config);
+    }
+
+    public Connection getConnection() throws SQLException {
+        return hikari.getConnection();
+    }
+
+    public boolean initDatabaseTables() throws SQLException {
+        try(Connection conn = getConnection()){
+            try(PreparedStatement ps = conn.prepareStatement("CREATE TABLE IF NOT EXISTS playerdata (uuid VARCHAR(255))")){
+                ps.executeUpdate();
+                return true;
+            }
+        }
+    }
+}
